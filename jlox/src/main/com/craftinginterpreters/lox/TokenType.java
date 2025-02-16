49dc26ffa971084453ec5a64bd43c9128f2fb545
Alex Jones <alexjajones1@gmail.com>
2025-02-16 11:14:58 +0000

    Added single char parsing

--- jlox/src/main/com/craftinginterpreters/lox/TokenType.java
+++ jlox/src/main/com/craftinginterpreters/lox/TokenType.java
@@ -1,16 +1,20 @@
 package src.main.com.craftinginterpreters.lox;
 
 enum TokenType {
+	// Single-character tokens.
 	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
 	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
+	// One or two character tokens.
 	BANG, BANG_EQUAL,
 	EQUAL, EQUAL_EQUAL,
 	GREATER, GREATER_EQUAL,
 	LESS, LESS_EQUAL,
 
+	// Literals
 	IDENTIFIERS, STRING, NUMBER,
 
+	// Keywords
 	ADD, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
 	PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
 
